418909cb4cc18e67ed2e6c3bc4a8398cf5754f04
Jolly Hotte <jollyhotte@Jollys-Mac-Pro.local>
2013-10-21 20:23:36 +0200

    initial commit

--- process.R
+++ process.R
@@ -1,4 +1,25 @@
 install.packages('randomForest')
+require('randomForest')
+
+convertToFactors <- function(data){
+  data$SibSp.factor <- factor(data$SibSp)
+  data$Pclass.factor <- factor(data$Pclass)
+  data$Parch.factor <- factor(data$Parch)
+  convertToFactors <- data
+}
 
 train <- read.csv('train.csv')
 test <- read.csv('test.csv')
+
+test <- convertToFactors(test)
+train <- convertToFactors(train)
+train$Survived.factor <- as.factor(train$Survived)
+
+mod.logit <- glm(Survived.factor~Pclass.factor*Sex*Age+Fare+Embarked, data = train, family = binomial(link = "logit"))
+
+test$Survived <- predict(mod.logit, newdata=test,type = "response")
+test$Survived.binary <- 0
+test$Survived.binary[test$Survived>0.5] <- 1
+test$Survived <- test$Survived.binary
+
+write.table(test[,c("PassengerId","Survived")], file="predictionslogit.csv",sep = ",",row.names=FALSE,col.names = TRUE)
